eeea7c288a841bc82e984edb70c7c4afe3673a44
Marcel Onyeukwu <onyeukwum4@gmail.com>
2023-11-15 05:29:31 -0800

    Header file

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list val);
+int print_string(va_list val);
+int _strlen(char *str);
+int _strlenc(const char *str);
+int print_int(va_list arg);
+int print_37(void);
