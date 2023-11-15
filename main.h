56df5ddd774eb2bd12458f3dc5c7603a5d5df387
Marcel Onyeukwu <onyeukwum4@gmail.com>
2023-11-15 11:17:33 -0800

    header file

--- main.h
+++ main.h
@@ -6,8 +6,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-
-
 /**
  * struct format - converter for printf
  * @ph: type char pointer of the specifier
@@ -23,9 +21,9 @@ typedef struct format
 
 int *_strcpy(char *dest, char *src);
 int print_pointer(va_list val);
-int print_unsigned(va_list args);
+int print_unsigned(va_list arg);
 int print_hex_extra(unsigned long int num);
-int _strlenc(const char *s);
+int _strlenc(const char *str);
 int print_HEX_extra(unsigned int num);
 int print_exc_string(va_list val);
 int print_HEX(va_list val);
@@ -34,13 +32,13 @@ int print_oct(va_list val);
 int print_bin(va_list val);
 int print_revs(va_list args);
 int print_rot13(va_list args);
-int print_i(va_list args);
+int print_int(va_list arg);
 int print_d(va_list args);
-int _strlen(char *s);
-int rev_string(char *s);
+int _strlen(char *str);
+int rev_string(char *str);
 int print_37(void);
-int print_c(va_list val);
-int print_s(va_list val);
+int printf_char(va_list val);
+int printf_string(va_list val);
 int _putchar(char c);
 int _printf(const char *format, ...);
 
