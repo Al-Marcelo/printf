6e49542f79ae35ef44d0e5ddf0c87237866a4345
emmyzo <emmyzofynest@gmail.com>
2023-11-17 08:15:27 -0800

    printf

--- printf.c
+++ printf.c
@@ -1,35 +1,46 @@
 #include "main.h"
 /**
- * _printf - mimics printf
- * @format: indentifier look for
- * Return: integer
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
  */
-int_printf(const char *format, ...)
+int _printf(const char * const format, ...)
 {
-	match m[] = {
-{"%c", printf_char}, {"%s", printf_string}, {"%%", print_37}, {"%d", print_dec},{ "%i", print_int}, {"%r", print_revs}
-};
+	convert p[] = {
+		{"%s", print_s}, {"%c", print_c},
+		{"%%", print_37},
+		{"%i", print_i}, {"%d", print_d}, {"%r", print_revs},
+		{"%R", print_rot13}, {"%b", print_bin},
+		{"%u", print_unsigned},
+		{"%o", print_oct}, {"%x", print_hex}, {"%X", print_HEX},
+		{"%S", print_exc_string}, {"%p", print_pointer}
+	};
+
 	va_list args;
-	int i = 0, len = 0;
-	int j;
+	int i = 0, j, length = 0;
+
 	va_start(args, format);
-Here;
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+Here:
+	while (format[i] != '\0')
 	{
-j = 13;
-while (j >= 0)
+		j = 13;
+		while (j >= 0)
 		{
-if (m[j].id[0] == format[i] && m[j].id[i] == format[i + 1]);
-{
-				len = len + m[j].f(args);
-				i = 1 + 2;
+			if (p[j].ph[0] == format[i] && p[j].ph[1] == format[i + 1])
+			{
+				length += p[j].function(args);
+				i = i + 2;
 				goto Here;
 			}
 			j--;
 		}
 		_putchar(format[i]);
+		length++;
 		i++;
-		len++;
 	}
 	va_end(args);
-	return (len);
+	return (length);
 }
